ddbeb01c7c59800328185ae9b3539d44339920b7
Unknown <profe.speranza5@gmail.com>
2020-09-08 14:18:55 -0300

    copio programa

---
+++ ConsoleApp2/ConsoleApp2/Carta.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Carta
+    {
+        //atributos de la clase 
+        int palo;
+        int numero;
+        String[] palos = { "Oro","Copas","Espadas","Bastos" };
+
+        public Carta(int palo, int numero)//constructor de la clase, es el que inicializa al objeto, recibe por parametro el palo y el nuemero
+        {
+            //numeros son del 1 al 12
+            //palos 0 al 3
+            if (numero <1 || numero > 12)//verifico en el constructor que los datos sean correctos, si son correctos asigno
+            {
+                Console.WriteLine("Numero incorrecto");
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+            else
+            {
+                this.numero = numero; //el this.numero se refiere al atributo numero de la clase, el numero solo se refiere al numero que le paso por parametro
+            }
+            if (palo>4 || palo <0)
+            {
+                Console.WriteLine("palo incorrecto"); Console.ReadKey();
+                Environment.Exit(2);
+            }
+            else {
+                this.palo = palo;
+            }
+        }
+
+        public void escribeCarta() //metodo de la clase carta que llamo para escribir el numero y el palo de la carta
+        {
+            Console.WriteLine(numero.ToString() + " de " + palos[palo]);
+        }
+
+        public int getNumero() //metodo de la clase carta que llamo para obtener el numero de la carta sin que pueda acceder al atributo en si.
+        {
+            return this.numero;
+        }
+
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, palo;
+
+            Console.WriteLine("Ingrese el numero de la carta");
+            numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el palo de la carta, 0 para oro, 1 para copa, 2 para espada y 3 para basto");
+            palo = int.Parse(Console.ReadLine());
+
+            Carta naipe = new Carta(palo, numero); //declaro el objeto naipe del tipo Carta, para inicializarlo y que no tenga basura
+                                                   //para inicializar pongo la palabra clave new y despues llamo al constructor
+                                                   //con los parametros que precisa
+
+            naipe.escribeCarta(); //llamo al metodo escribeCarta del objeto naipe (que es de la clase Carta)
+
+            Console.ReadKey();
+        }
+    }
+}
